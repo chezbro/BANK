487cf99ac39772109669a786b0376a20f4bcc97d
Eric Chesbrough <EricChesbrough@gmail.com>
2014-01-27 22:34:43 -0600

    added images

--- Gemfile
+++ Gemfile
@@ -2,6 +2,7 @@ source 'https://rubygems.org'
 
 gem 'nokogiri'
 gem 'mechanize'
+gem "google_image_api"
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.0.2'
--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,15 +2,38 @@ require 'rubygems'
 require 'nokogiri'
 require 'open-uri'
 require 'mechanize'
+require 'google_image_api'	
 
 class StaticPagesController < ApplicationController
 	def home_page
 		agent = Mechanize.new
 		agent.get("http://www.celebritynetworth.com/random")
 		# random = agent.page.link_with(text: "Random Celebrity Profile")
-		@title = agent.page.at("title").text
 		@worth = agent.page.at(".networth_amount_value").text
 		@body = agent.page.at(".networth_content p").text
+
+		@title = agent.page.at("title").text
+
+		name = @title.split(/\W+/)
+		first_name = name[0]
+		last_name = name[1]
+		@full_name = first_name + ' ' + last_name
+
+
+	  result = GoogleImageApi.find("#{@full_name}", {
+	    :imgsz => "medium",
+	    :rsz => 8,
+	    :start => 8,
+	    :imgtype  => "face",
+	    :as_filetype => "jpg"
+	  })
+
+	  result = result.images.first
+	  @url = result["unescapedUrl"]
+	  
+  # result.images.first do |img|
+  #   puts img['url']
+  # end
 
 
 
